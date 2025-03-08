ef609987bab03063f2aaafd00c9b04546d9c5699
Kale Moskowitz <kalebug@gmail.com>
2025-03-08 12:14:18 -0800

    testing

---
+++ StickerSmash/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="Home" options={{title: 'Home'}} />
+      <Stack.Screen name="About" options={{title: 'About'}} />
+    </Stack>
+  );
+}
