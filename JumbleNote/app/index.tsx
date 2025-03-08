65f86929192b8c9b852701690e05d906fd3729ff
Tmhduc <duc_ta@sfu.ca>
2025-03-08 12:16:41 -0800

    setting up

---
+++ JumbleNote/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text style={style.text}>I fuck with it</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#252923",
+  },
+  text: {
+    color: "white",
+  }
+});
